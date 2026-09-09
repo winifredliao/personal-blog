ea07ae6dc216ed29b554ffc82781d7f37fbd10d9
Winnie <bluestar3426@gmail.com>
2026-09-09 22:52:28 +0800

    Dockerize frontend and backend with docker-compose

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./blog.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
